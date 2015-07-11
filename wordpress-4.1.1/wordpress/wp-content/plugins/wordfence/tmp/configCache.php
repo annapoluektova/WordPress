6748a3be19e7524c3be9796b297a3df65e061df8
annapoluektova <annapoluektova@yahoo.ca>
2015-07-10 20:06:41 -0400

    init files

---
+++ wordpress-4.1.1/wordpress/wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:7:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:192:"f59d72d0bdd1c5acf4a6660a6bc5f18b1fc44eae5b0fec74b5f9cec66931d3883416aa0f9e8c82642ff48d2a83123120236c3e9e88f0f1adc15c36f2b9207059da12e1ed9fb3dd02257e8d60b5d3794fd69b62c7d934dcce6192814214c6150b";s:9:"howGetIPs";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:6:"encKey";s:16:"3062000025f30000";}
